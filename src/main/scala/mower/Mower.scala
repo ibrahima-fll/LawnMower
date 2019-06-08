f0b3b8ad26950330822d0bb0f6d545ed77fb31af
Ibrahima FALL <sylfarion@icloud.com>
2019-06-09 00:39:09 +0200

    [ADD] toString for mowers

--- src/main/scala/mower/Mower.scala
+++ src/main/scala/mower/Mower.scala
@@ -3,11 +3,12 @@ package mower
 import scala.annotation.tailrec
 
 case class Mower(position: Position, actions: Seq[Action]) {
+  override lazy val toString = s"Mower(${position.x}, ${position.y}, ${position.headDirection})"
+
   def move(action: Action): Mower = this.copy(
     position = position.move(action),
     actions = actions.drop(1)
   )
-
 
   //TODO: do not move if next move will put the mower outside the lawn.
   def executeActions: Mower = {
